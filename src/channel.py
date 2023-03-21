f41f5c6ea52b9123ba587f2469b460bb474d60f6
Dmitry Zakhs <d.zakhs@gmail.com>
2023-03-21 21:54:02 +0300

    homework2

--- src/channel.py
+++ src/channel.py
@@ -2,17 +2,35 @@ import json
 import os
 from googleapiclient.discovery import build
 
+
+api_key = os.getenv("API_KEY")
+youtube = build('youtube', 'v3', developerKey=api_key)
+
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = channel['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscribers = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.views = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        api_key = os.getenv("API_KEY")
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel_id=self.channel_id
-        channel = youtube.channels().list(id=channel_id, part='snippet, statistics').execute()
+
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet, statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return youtube
+
+    def to_json(self, name):
+        with open(name, 'w', encoding='utf-8') as file:
+            json.dump(self.print_info(), file, indent=2, ensure_ascii=False)
+
