f74f623ec509234c9f631b1e8017050275e6ee38
Dmitry Zakhs <d.zakhs@gmail.com>
2023-03-29 00:13:57 +0300

    add homework 4

---
+++ src/video.py
@@ -0,0 +1,25 @@
+import os
+from googleapiclient.discovery import build
+
+
+class Video:
+    api_key = os.getenv("API_KEY")
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    def __init__(self, video_id):
+        self.video_id = video_id
+        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                         id=self.video_id).execute()
+        self.video_title = self.video_response['items'][0]['snippet']['title']
+        self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
+        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+        self.likes_count = self.video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return self.video_title
+
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_ids):
+        super().__init__(video_id)
+        self.playlist_ids = playlist_ids
