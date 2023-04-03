e41f3ffdd52dcf539975b4911153d83c792847b4
Dmitry Zakhs <d.zakhs@gmail.com>
2023-04-03 23:37:54 +0300

    add homework 6

--- src/video.py
+++ src/video.py
@@ -6,16 +6,22 @@ class Video:
     api_key = os.getenv("API_KEY")
     youtube = build('youtube', 'v3', developerKey=api_key)
     def __init__(self, video_id):
-        self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+        try:
+            self.video_id = video_id
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                          id=self.video_id).execute()
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.likes_count = self.video_response['items'][0]['statistics']['likeCount']
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except:
+            self.title = None
+            self.like_count = None
+            print("IndexError")
+
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 
